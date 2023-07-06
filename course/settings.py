b9e2e8fc73aedc4749dc7bec0436fe7afc5cd867
Franco Sparn <47465437+francosparn@users.noreply.github.com>
2023-07-06 19:14:16 -0300

    Update settings.py

--- course/settings.py
+++ course/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+*w4v(yiy+%4#vwe@_$0(1@zzx6tl*wr0%hgob+qe48io9efdy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
